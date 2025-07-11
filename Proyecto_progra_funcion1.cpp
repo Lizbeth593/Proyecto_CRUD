b40d0fcd2b2bf0bcbbb851153604f680cd6863a4
Lizbeth Sanchez <domenica.sanchez01@epn.edu.ec>
2025-07-10 19:40:48 -0500

    Primer commit , funcion 1 crear producto

---
+++ Proyecto_progra_funcion1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Producto {
+    int id;
+    string nombre;
+    string categoria;
+    double precio;
+};
+
+
+void crearProducto() {
+    ofstream archivo("productos.txt", ios::app);
+    Producto prod;
+
+    cout << "ID del producto
+	: "; cin >> prod.id;
+    cout << "Nombre del producto: "; cin.ignore(); getline(cin, prod.nombre);
+    cout << "Categoría: "; getline(cin, prod.categoria);
+    cout << "Precio (USD): "; cin >> prod.precio;
+
+    archivo << prod.id << "," << prod.nombre << "," << prod.categoria << "," << prod.precio << endl;
+    archivo.close();
+}
