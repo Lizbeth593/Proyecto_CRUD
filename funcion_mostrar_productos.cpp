5332ce2f078b1cfcbcf3147e1c19204633971de3
Lizbeth593 <lizbeth59312@gmail.com>
2025-07-16 17:38:34 -0500

    funcion_mostrar_productos_para el buscar.cpp

--- funcion_mostrar_productos.cpp
+++ funcion_mostrar_productos.cpp
@@ -1,22 +1,11 @@
-void mostrarProductos() {
-	cout << "------LISTA DE PRODUCTOS----- \n";
-    ifstream archivo("productos.txt");
-    string linea;
-
-    while (getline(archivo, linea)) {
-        cout << linea << endl;
-    }
-
-    archivo.close();
-}
-
 
 // para reutilizar al momento que se encuentra el producto y mostrarlo en este formato 
 // por que no se como hacer para ponerle en una sola funcion 
 
 void mostrar(const Producto& prod) {
-    cout << "ID: " << prod.id
-	     << " | Nombre: " << prod.nombre
-             << " | Categoría: " << prod.categoria 
-	     << " | Precio: $" << prod.precio << endl;
+    cout << "ID: " << prod.id << endl;
+    cout << "Nombre: " << prod.nombre << endl;
+    cout << "Categoría: " << prod.categoria << endl;
+    cout << "Precio: " << prod.precio << endl;
+    cout << "---------------------------\n";
 }
