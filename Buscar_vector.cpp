340d426c0a126a3411f58f30248e3e88e0ac6b4d
erickc07 <Lizbeth59312@gmail.com>
2025-07-24 15:14:06 -0500

    Funcion buscar vector LIZ

---
+++ Buscar_vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Producto {
+	int id;
+	string nombre;
+	string categoria;
+	double precio;
+};
+
+bool buscarProductoPorID(int idBuscar, Producto& prodEncontrado, const string& nombreArchivo = "productos.txt") {
+	ifstream archivo(nombreArchivo);
+	Producto prod;
+	
+	while (archivo >> prod.id) {
+		archivo.ignore();
+		getline(archivo, prod.nombre, ',');
+		getline(archivo, prod.categoria, ',');
+		archivo >> prod.precio;
+		archivo.ignore();
+		
+		if (prod.id == idBuscar) {
+			prodEncontrado = prod;
+			return true;
+		}
+	}
+	
+	return false;
+}
